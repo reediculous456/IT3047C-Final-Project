1c78c2338953d4ff3d1c47dfe2ee9cc39e1943d8
rosaricd <rosaricd@mail.uc.edu>
2021-04-18 13:18:42 -0400

    Added config data, other busywork
    - Added user to config
    - Added trail to config
    - Added generic TempData messages for create, edit, delete operations

--- HikingTrails/Models/Configuation/UserConfig.cs
+++ HikingTrails/Models/Configuation/UserConfig.cs
@@ -22,6 +22,14 @@ namespace HikingTrails.Models
                    LastName = "Reed",
                    Age = 20,
                    Bio = "Hello, my name is Wes and I'm a 3rd year IT student in the software and cyber tracks. I'm also in the IT accelerated program for my MSIT, and I play Trombone in the Bearcat Bands.",
+               },
+               new User
+               {
+                   UserId = 3,
+                   FirstName = "Christian",
+                   LastName = "Rosario",
+                   Age = 23,
+                   Bio = "Hello, my name is Christian and I'm a 3rd year IT student in the software development track. I don't go hiking regularly, but the few times I have gone were pretty fun."
                }
             );
         }
