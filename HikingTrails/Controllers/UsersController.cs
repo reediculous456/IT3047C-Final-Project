dea3d1a2fd168f51c1e4a24f3b2c19e018729b9e
Wesley Reed <reedws@ucmail.uc.edu>
2021-04-20 23:13:41 -0400

    code cleanup

--- HikingTrails/Controllers/UsersController.cs
+++ HikingTrails/Controllers/UsersController.cs
@@ -1,6 +1,5 @@
 ﻿using HikingTrails.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
